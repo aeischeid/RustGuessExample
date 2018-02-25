756695e57df6971c963b3a4f7e62c070bd90b5ae
aeischeid <a.eischeid@gmail.com>
2018-02-25 16:57:47 -0600

    add variable difficulty and more snark

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,41 @@ use std::cmp::Ordering;
 use rand::Rng;
 
 fn main() {
-    println!("Guess the number!");
+    println!("Let's play a guessing game!");
+    println!("---------------------------");
 
-    let secret_number = rand::thread_rng().gen_range(1, 101);
+    println!("I am going to think of a number.");
+    println!("But why don't you decide how hard this will be");
+
+    println!("my secret number will be between 1 and a number you pick...");
+    let mut got_top_range = false;
+    let mut top_range = 100;
+
+    while !got_top_range {
+        let mut top_range_input = String::new();
+        io::stdin().read_line(&mut top_range_input)
+            .expect("Failed to read line");
+
+        top_range = match top_range_input.trim().parse() {
+            Ok(num) => {
+                got_top_range = true;
+                num
+            },
+            Err(_) => {
+                println!("stop goofing - how aboout a number now?!");
+                continue;
+            },
+        };
+    }
+    guessin_time(&top_range);
+}
+
+fn guessin_time(top_range: &u32) {
+    println!("Ok, got it! I'm thinking of a number between 1 and {}:", top_range);
+    println!("How many guesses will it take for you to figure it out?");
+
+    let top_range = top_range + 1;
+    let secret_number = rand::thread_rng().gen_range(1, top_range);
     let mut guesses = 0;
     let mut next_guess = "";
     let pre_low_high = "^ Your guess was";
