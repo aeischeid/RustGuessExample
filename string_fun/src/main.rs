3425408cc320b97d16f3007194d8e0139a819f4e
aeischeid <a.eischeid@gmail.com>
2019-02-02 12:39:24 -0600

    magic string directionality

--- string_fun/src/main.rs
+++ string_fun/src/main.rs
@@ -15,6 +15,17 @@ fn main() {
     println!("similar to groovy gString, \"foo $var thing\" or js strings with `idont $know etc.` {}", s);
 
 
+    let s = "  English  ";
+    let firstLetter1 = s.trim_start().chars().next().unwrap();
+    assert_eq!('E', firstLetter1);
+    println!("first letting in LTR lang string {}", firstLetter1);
+
+    // how the eff does it know directionality?!?!?
+    let s = "  עברית  ";
+    let firstLetter2 = s.trim_start().chars().next().unwrap();
+    assert!('ע' == firstLetter2);
+    println!("first letting in RTL lang string {}", firstLetter2);
+
     // println!("{:?}", );
     for c in "नमस्ते".chars() {
         println!("{}", c);
