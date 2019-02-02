d0b034ffe980b8b6e7c50406ded89a75b3d98dd1
aeischeid <a.eischeid@gmail.com>
2019-02-02 11:37:31 -0600

    string interpolation and escaping chars

--- string_fun/src/main.rs
+++ string_fun/src/main.rs
@@ -5,6 +5,17 @@ fn main() {
 
     println!("Hello, {}!", s);
 
+    let s1 = String::from("tic");
+    let s2 = String::from("tac");
+    let s3 = String::from("toe");
+
+    let s = format!("{}-{}-{}", s1, s2, s3);
+
+    println!("format is just Rust's string concatenator");
+    println!("similar to groovy gString, \"foo $var thing\" or js strings with `idont $know etc.` {}", s);
+
+
+    // println!("{:?}", );
     for c in "नमस्ते".chars() {
         println!("{}", c);
     }
