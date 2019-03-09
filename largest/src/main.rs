3e448eda28ff63c1a3ede06048795b9e812cdb85
aeischeid <a.eischeid@gmail.com>
2019-03-09 14:50:47 -0600

    considering generics in largest

--- largest/src/main.rs
+++ largest/src/main.rs
@@ -10,6 +10,18 @@ fn largest(list: &[i32]) -> i32 {
     largest
 }
 
+// fn largest<T>(list: &[T]) -> T {
+//     let mut largest = list[0];
+//
+//     for &item in list.iter() {
+//         if item > largest {
+//             largest = item;
+//         }
+//     }
+//
+//     largest
+// }
+
 fn main() {
     let number_list = vec![34, 50, 25, 100, 65];
 
@@ -20,4 +32,9 @@ fn main() {
 
     let result = largest(&number_list);
     println!("The largest number is {}", result);
+
+    // let char_list = vec!['y', 'm', 'a', 'q'];
+    //
+    // let result = largest(&char_list);
+    // println!("The largest char is {}", result);
 }
