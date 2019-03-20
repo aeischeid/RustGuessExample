4f80662b62cece0987ba3515cd59dae6bdd6e03c
aeischeid <a.eischeid@gmail.com>
2019-03-20 00:36:37 -0500

    add tests for can_hold on rectangle struct

--- rectangle/src/main.rs
+++ rectangle/src/main.rs
@@ -55,3 +55,25 @@ fn main() {
 // fn area(rectangle: &Rectangle) -> u32 {
 //     rectangle.width * rectangle.height
 // }
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn larger_can_hold_smaller() {
+        let larger = Rectangle { width: 8, height: 7 };
+        let smaller = Rectangle { width: 5, height: 1 };
+
+        assert!(larger.can_hold(&smaller));
+    }
+
+    #[test]
+    fn smaller_cant_hold_larger() {
+        let larger = Rectangle { width: 8, height: 7 };
+        let smaller = Rectangle { width: 5, height: 1 };
+
+        assert!(!smaller.can_hold(&larger));
+    }
+}
